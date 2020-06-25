0183eca533e3dd271fa1fb7534bea2d846f8f12b
Joseph Johnson <jwjbadger@gmail.com>
2020-06-25 16:40:37 -0500

    Add ng-bootstrap styling to make todos look better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodosComponent, TodoComponent],
+  imports: [NgbModule, BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
