e827aaa6be7a5169e398dd246961c88055d5d071
Joseph Johnson <jwjbadger@gmail.com>
2020-07-01 12:08:54 -0500

    Add creation of new tasks on projects

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Project } from 'src/app/store/models/project.model';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/store/models/user.model';
+import { ProjectService } from 'src/app/services/project-service.service';
+import { AddTask } from 'src/app/store/actions/projects.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-project',
@@ -9,7 +14,32 @@ import { Project } from 'src/app/store/models/project.model';
 export class ProjectComponent implements OnInit {
   @Input() project: Project;
 
-  constructor() {}
+  projects$: Observable<Project[]>;
+  projects: Project[];
+  index: number;
 
-  ngOnInit(): void {}
+  constructor(
+    private projectService: ProjectService,
+    private store: Store<{ user: User; projects: Project[] }>
+  ) {}
+
+  ngOnInit(): void {
+    this.projects$ = this.store.select((state) => state.projects);
+    this.projects$.subscribe((store) => {
+      this.projects = store;
+      this.index = store.indexOf(this.project);
+      this.projectService.putProject(this.project).toPromise();
+    });
+  }
+  createNew(title: string, description: string) {
+    this.store.dispatch(
+      AddTask({
+        payload: {
+          title: title,
+          description: description,
+          projectIndex: this.index,
+        },
+      })
+    );
+  }
 }
